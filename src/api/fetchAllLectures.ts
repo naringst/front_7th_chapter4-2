8302985730e554656174ef2ecc82f79b832958b3
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-12-24 21:37:42 +0900

    perf: 클로저를 활용해 서버 데이터 캐싱 처리

--- src/api/fetchAllLectures.ts
+++ src/api/fetchAllLectures.ts
@@ -1,29 +1,34 @@
 import axios, { AxiosResponse } from 'axios';
 import { Lecture } from '../types';
 
-const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
-const fetchLiberalArts = () =>
-  axios.get<Lecture[]>('/schedules-liberal-arts.json');
+const createCachedFetcher = <T>(fetcher: () => Promise<T>) => {
+  let cache: Promise<T> | null = null;
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-// NOTE: Promise.all([Promise1, Promise2,,, ])와 같은 형태여야 한다.
-export const fetchAllLectures = async () => {
-  const fetchMap: Map<string, () => Promise<AxiosResponse<Lecture[]>>> =
-    new Map();
+  return () => {
+    if (!cache) {
+      console.log('API Call (새 요청)', performance.now());
+      cache = fetcher();
+    } else {
+      console.log('API Call (캐시 사용)', performance.now());
+    }
+    return cache;
+  };
+};
 
-  if (!fetchMap.has('majors')) {
-    fetchMap.set('majors', fetchMajors);
-  }
-  if (!fetchMap.has('liberalArts')) {
-    fetchMap.set('liberalArts', fetchLiberalArts);
-  }
+const fetchMajors = createCachedFetcher(() =>
+  axios.get<Lecture[]>('/schedules-majors.json'),
+);
+const fetchLiberalArts = createCachedFetcher(() =>
+  axios.get<Lecture[]>('/schedules-liberal-arts.json'),
+);
 
+export const fetchAllLectures = async (): Promise<
+  AxiosResponse<Lecture[]>[]
+> => {
   return await Promise.all([
-    (console.log('API Call 1', performance.now()), fetchMajors()),
-    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), fetchMajors()),
-    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), fetchMajors()),
-    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
+    fetchMajors(),
+    fetchLiberalArts(),
+    fetchMajors(),
+    fetchLiberalArts(),
   ]);
 };
